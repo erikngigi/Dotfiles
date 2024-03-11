5e04c92b6bda79f03bf5fe1bc6b8e0170ad48dd4
ErikNgigi <ericmosesngigi@gmail.com>
2024-03-11 14:37:01 +0300

    delete unused scripts

--- scripts/.scripts/ip-address.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PUBLIC_IP=$(curl -s https://api.ipify.org)
-
-echo "Your public IP address is: $PUBLIC_IP"
--- scripts/.scripts/lvds-only.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output LVDS --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-0 --off --output VGA-0 --off
